2f711e2ed1d1446746820cb2cde844c795b9cd9e
Gretron <89054564+Gretron@users.noreply.github.com>
2024-06-09 20:22:23 -0900

    chore: setup base design system using tailwindcss

--- src/packages/ui/src/button.tsx
+++ src/packages/ui/src/button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
+import "src/assets/css/globals.css";
 
 interface ButtonProps {
   children: ReactNode;
@@ -11,7 +12,7 @@ interface ButtonProps {
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={"bg-primary-"}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
