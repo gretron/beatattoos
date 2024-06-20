14303b42b91412184da062d9515698c7b10cd547
Gretron <89054564+Gretron@users.noreply.github.com>
2024-06-19 20:43:58 -0900

    test(admin): add register e2e tests

--- src/apps/admin/src/tests/auth/register.spec.ts
+++ src/apps/admin/src/tests/auth/register.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from "@playwright/test";
+import { db } from "~/lib/db";
 
 test("registration process", async ({ page }) => {
-  await page.goto("http://localhost:3000/auth");
+  const { count } = await db.user.deleteMany({
+    where: { role: { equals: "ADMIN" } },
+  });
 
-  await expect(page).toHaveTitle(/beatattoos/);
+  await page.goto("/auth");
+
+  await page.getByRole("button", { name: "First Time Setup" }).click();
+
+  await expect(page).toHaveURL(/auth\/token/);
+
+  await page.getByRole("textbox").fill(process.env.ADMIN_TOKEN ?? "");
+  await page.getByRole("button", { name: "Validate" }).click();
+
+  await expect(page).toHaveURL(/auth\/register/);
 });
